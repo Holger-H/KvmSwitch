3370f6fe75a209b1866a243c7f81abec12c3b607
Holger-H <82891537+Holger-H@users.noreply.github.com>
2021-05-07 17:36:49 +0200

    migrated projects to SDK format

--- src/KvmSwitch/Properties/AssemblyInfo.cs
+++ src/KvmSwitch/Properties/AssemblyInfo.cs
@@ -4,18 +4,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using log4net.Config;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("KvmSwitch")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KvmSwitch")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -39,20 +27,6 @@ using log4net.Config;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("KvmSwitch.Tests")]
 
